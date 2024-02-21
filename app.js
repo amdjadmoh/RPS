908ff31af19db61218cc8b05a05b6c2d9a80ec8c
amdjad <ma.mehdi@esi-sba.dz>
2024-02-21 17:33:42 +0100

    fixed some bugs with css and js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const choices=['rock','paper','scissor']
 function getComputerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)]
+    return choices[Math.floor(Math.random()*3)]
 }
 function getplayerselection(){
     return prompt('Rock or Paper or Scissor').toLowerCase()
 }
 function pickwinner(p1,p2){
     if (p1==p2) { } 
-    else if (p1=='rock' && p2=='paper') { updateScore(true) ;}
-    else if (p1=='paper' && p2=='rock') { updateScore(false);}
+    else if (p1=='rock' && p2=='paper') { updateScore(false) ;}
+    else if (p1=='paper' && p2=='rock') { updateScore(true);}
     else if (p1=='scissor' && p2=='paper') { updateScore(true);}
     else if (p1=='paper' && p2=='scissor') {updateScore(false);}
     else if (p1=='rock' && p2=='scissor') { updateScore(true);}
@@ -27,10 +27,9 @@ score.textContent=parseInt(score.textContent)+1;
 }
 }
 function showMove(playerMove,computerMove){
-    let PlayerImage=document.querySelector(".playerMoveImage");
-  PlayerImage.innerHTML = `<img src="img/${playerMove}.png" alt="${playerMove}">`;
-  let computerImage=document.querySelector(".computerMoveImage");
-  computerImage.innerHTML = `<img src="img/${computerMove}.png" alt="${computerMove}">`;
+    let Image=document.querySelector(".moves");
+  Image.innerHTML = `<img src="img/${playerMove}.png" alt="${playerMove}">`;
+  Image.innerHTML += `<img src="img/${computerMove}.png" alt="${computerMove}">`;
 }
 
 
